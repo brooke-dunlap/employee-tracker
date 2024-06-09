2ca86ef127f2655865ef8981c9d44c4bd59c3deb
Brooke <brookedunlap97@gmail.com>
2024-06-09 00:33:43 -0500

    Fixed errors in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+--@block
 INSERT INTO department (name)
 VALUES 
     ('Marketing'), 
@@ -11,21 +12,21 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
     ('marketing analyst', 60000, 1),
-    ('marketing assistant', 75000, 1)
-    ('marketing director', 90000, 1)
+    ('marketing assistant', 75000, 1),
+    ('marketing director', 90000, 1),
     ('chief finance officer', 150000, 2),
     ('financial adviser', 80000, 2),
-    ('customer service representative', 50000, 5),
+    ('customer service rep', 50000, 5),
     ('customer service manager', 65000, 5),
     ('information security analyst', 85000, 3),
     ('network administrator', 85000, 3),
     ('human resources specialist', 70000, 6),
     ('human resources manager', 85000, 6),
-    ('sales representative', 80000, 4),
+    ('sales rep', 80000, 4),
     ('account manager', 90000, 4),
     ('regional sales manager', 110000, 4);
 
-==@block
+--@block
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
     ('Sara', 'Pratt', 1),
@@ -41,7 +42,7 @@ VALUES
     ('Quinn', 'Holmes', 11),
     ('Dani', 'Frye', 12),
     ('Sarai', 'Cline', 13),
-    ('Luis', 'Richardson', 14);
+    ('Luis', 'Richardson', 14),
     ('Jeffery', 'Hammond', 6),
     ('Kyle', 'Atkins', 6),
     ('Adan', 'Guerra', 12);

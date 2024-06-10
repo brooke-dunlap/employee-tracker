fa66f5c9745d14fc050581896d2001ac0dedc6a7
Brooke <brookedunlap97@gmail.com>
2024-06-10 12:55:17 -0500

    Used inquirer to prompt user for selection regarding their desired action. Switch statement is used to excecute function depended on user's choice

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: ['View all employees', 'Add employee', 'View all roles', 'Update employee role', 'Add role', 'View all departments', 'Add department'],
+    },
+]);
+
+switch (action) {
+    case 'View all employees':
+        viewAllEmployees();
+        break;
+    case 'Add employee':
+        addEmployee();
+        break;
+    case 'View all roles':
+        viewAllRoles();
+        break;
+    case 'Update employee role':
+        updateRole();
+        break;
+    case 'Add role':
+        addRole();
+        break;
+    case 'View all departments':
+        viewAllDepartments();
+        break;
+    case 'Add department':
+        addDeparment();
+}
+
+function viewAllEmployees(){};
+function addEmployee(){};
+function viewAllRoles(){};
+function addRole(){};
+function updateRole(){};
+function viewAllDepartments(){};
+function addDeparment(){};
